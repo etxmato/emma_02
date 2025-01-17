4f4879f9eaded549d1f02526717c8640c852b71c
Marcel van Tongeren <marcel.v.tongeren@gmail.com>
2025-01-17 08:56:25 +0100

    Update cdp1877.h

--- src/cdp1877.h
+++ src/cdp1877.h
@@ -40,4 +40,48 @@ private:
 };
 
 
-#endif  // CDP1877_H
+#endif  // CDP1855_H
+
+#ifndef CDP1855_H
+#define CDP1855_H
+
+class Cdp1855Instance
+{
+public:
+    Cdp1855Instance(int cdp1855Number);
+    ~Cdp1855Instance() {};
+  
+    void configureCdp1855(Cdp1855Configuration cdp1855Configuration);
+    bool ioGroupCdp1855(int ioGroup);
+
+    void requestInterrupt(int type, bool state, int picNumber);
+    int findInterrupt();
+    void devWrite(Word address, Byte value);
+    void writeMask(Byte value);
+    void writePage(Byte value);
+    void writeControl(Byte value);
+    int devRead(Word address, int function);
+    bool readMien() {return mien_;};
+    Byte readStatus();
+    Byte readPolling();
+    Byte readVector(Word address, int function);
+
+private:
+    int cdp1855Number_;
+
+    Cdp1855Configuration cdp1855Configuration_;
+    
+    bitset<8> mask_;
+    Byte page_;
+    Byte interval_;
+    Byte vectorAddressLow_;
+    bitset<8> status_;
+    
+    int vectorByteCounter_;
+    int interruptType[8];
+    
+    bool mien_;
+};
+
+
+#endif  // CDP1855_H
