cd55a5deb60c8a785568c6a809717e07a99c395d
Marcel van Tongeren <marcel.v.tongeren@gmail.com>
2025-02-02 09:44:13 +0100

    Update cdp1854.cpp

--- src/cdp1854.cpp
+++ src/cdp1854.cpp
@@ -37,16 +37,6 @@ Cdp1854Instance::Cdp1854Instance(int cdp1854Number)
 {
     cdp1854Number_ = cdp1854Number;
     
-    status_ = 0xff;
-    mask_ = 0;
-    page_ = 0;
-    interval_ = 0;
-    
-    vectorAddressLow_ = 0;
-    vectorByteCounter_ = 0;
-    
-    for (int i=0; i<8; i++)
-        interruptType[i] = 0;
 }
 
 void Cdp1854Instance::configureCdp1854(Cdp1854Configuration cdp1854Configuration)
@@ -56,9 +46,24 @@ void Cdp1854Instance::configureCdp1854(Cdp1854Configuration cdp1854Configuration
     if (cdp1854Number_ > 0)
         cdp1854NumberString.Printf(" %d", cdp1854Number_);
     
-    p_Main->configureMessage(&cdp1854Configuration.ioGroupVector, "CDP1854 Programmable Interrupt Controller" + cdp1854NumberString);
+    p_Main->configureMessage(&cdp1854Configuration.ioGroupVector, "CDP1854 UART" + cdp1854NumberString);
     
+    p_Computer->setOutType(&cdp1854Configuration.ioGroupVector, cdp1854Configuration.out, UART1854_LOAD_TRANSMITTER_OUT, "load transmitter");
+    p_Computer->setInType(&cdp1854Configuration.ioGroupVector, cdp1854Configuration.in, UART1854_READ_RECEIVER_IN, "read receiver");
+    p_Computer->setOutType(&cdp1854Configuration.ioGroupVector, cdp1854Configuration.control, UART1854_LOAD_CONTROL_OUT, "load control");
+    p_Computer->setInType(&cdp1854Configuration.ioGroupVector, cdp1854Configuration.status, UART1854_READ_STATUS_IN, "read status");
+    if (currentComputerConfiguration.cdp1854Configuration.efInterrupt.flagNumber != -1)
+        p_Computer->setEfType(&cdp1854Configuration.ioGroupVector, cdp1854Configuration.efInterrupt, VIDEO_TERMINAL_EF_INTERRUPT, "UART interrupt");
+    if (currentComputerConfiguration.cdp1854Configuration.ef.flagNumber != -1)
+    {
+        p_Computer->setEfType(&cdp1854Configuration.ioGroupVector, cdp1854Configuration.ef, VIDEO_TERMINAL_EF, "serial input");
+        uartEf_ = true;
+    }
+    p_Computer->setCycleType(CYCLE_TYPE_VIDEO_TERMINAL, VIDEO_TERMINAL_CYCLE);
+
     p_Main->message("");
+
+    rs232_ = 0;
 }
 
 bool Cdp1854Instance::ioGroupCdp1854(int ioGroup)
