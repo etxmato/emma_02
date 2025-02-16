d5eadf60f2af81512baa93cea3d3cd3e6bc0f936
Marcel van Tongeren <marcel.v.tongeren@gmail.com>
2025-02-16 11:04:12 +0100

    CDP1877 cleanup

--- src/cdp1877.cpp
+++ src/cdp1877.cpp
@@ -59,13 +59,21 @@ void Cdp1877Instance::configureCdp1877(Cdp1877Configuration cdp1877Configuration
     p_Main->configureMessage(&cdp1877Configuration.ioGroupVector, "CDP1877 Programmable Interrupt Controller" + cdp1877NumberString);
     
     wxString printBuffer;
-    printBuffer.Printf("	Main interrupt enable address: %04X with bit mask: %02X", cdp1877Configuration.writeMien.portNumber[0], cdp1877Configuration.writeMien.mask);
+    printBuffer.Printf("	Write main interrupt enable address: %04X with bit mask: %02X", cdp1877Configuration.writeMien.portNumber[0], cdp1877Configuration.writeMien.mask);
     p_Main->message(printBuffer);
-    printBuffer.Printf("	Write mask register address: %04X with address mask: %04X", cdp1877Configuration.writeMask.portNumber[0], cdp1877Configuration.writeMien.addressMask);
+    printBuffer.Printf("	Write mask register address: %04X with address mask: %04X", cdp1877Configuration.writeMask.portNumber[0], cdp1877Configuration.writeMask.addressMask);
+    p_Main->message(printBuffer);
+    printBuffer.Printf("	Write control register address: %04X with address mask: %04X", cdp1877Configuration.writeControl.portNumber[0], cdp1877Configuration.writeControl.addressMask);
+    p_Main->message(printBuffer);
+    printBuffer.Printf("	Write page register address: %04X with address mask: %04X", cdp1877Configuration.writePage.portNumber[0], cdp1877Configuration.writePage.addressMask);
+    p_Main->message(printBuffer);
+    printBuffer.Printf("	Read interrupt request address: %04X with bit mask: %02X", cdp1877Configuration.readIrq.portNumber[0], cdp1877Configuration.readIrq.mask);
     p_Main->message(printBuffer);
     printBuffer.Printf("	Read status register address: %04X with address mask: %04X", cdp1877Configuration.readStatus.portNumber[0], cdp1877Configuration.readStatus.addressMask);
     p_Main->message(printBuffer);
     printBuffer.Printf("	Read polling register address: %04X with address mask: %04X", cdp1877Configuration.readPolling.portNumber[0], cdp1877Configuration.readPolling.addressMask);
+    p_Main->message(printBuffer);
+    printBuffer.Printf("	Read vector / long branch address: %04X with address mask: %04X", cdp1877Configuration.readVector.portNumber[0], cdp1877Configuration.readVector.addressMask);
     p_Main->message(printBuffer);
 
     p_Main->message("");
